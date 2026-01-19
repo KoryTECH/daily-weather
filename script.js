fec83f233cc3f7855cf57a59f446073cd611e09a
KoryTECH <korylinemedia@gmail.com>
2026-01-19 22:20:17 +0100

    5% javaScript done

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const OPENWEATHER_KEY = `5efc126ec2af885ecd3006733142d32b`
 
 let lat;
 let lon;
-
+function getWeatherInfoOnLoad(){
 window.onload = function () {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
@@ -14,15 +14,30 @@ window.onload = function () {
         .then(response => response.json())
         .then(data => {
             console.log(data)
-            document.querySelector('#location').textContent = data.city.name;
-            document.querySelector('#date').textContent = data.list[0].dt_txt;
+            const date = data.list[0].dt_txt.split(' ')[0]
+            const locationName = data.city.name;
+            const minRange = data.list[0].main.temp_min;
+            const maxRange = data.list[0].main.temp_max;
+            const description = data.list[0].weather[0].description
+            
+            let list = data.list;
+            console.log(list[10])
+            document.getElementById('main-temp').innerText  = data.list[0].main.temp;
+            document.getElementById('location').innerText = locationName;
+            document.getElementById('date').innerText = date;
+            document.getElementById('temp-description').innerText = description;
+            document.getElementById('temp-range').innerText = `H:${maxRange} L:${minRange}`;
+
+          
+
         })
       });
     } else {
       console.log("Geolocation is not supported by this browser.");
     }
     
-}
+}}
+getWeatherInfoOnLoad()
 // console.log(`lat:${lat}, lon:${lon}`)
 
 // fetch(`${OPENWEATHER_URL}?lat=${lat}&lon=${lon}&appid=${OPENWEATHER_KEY}&units=metric`)
