641c5a5d0c52333efe90df52c04c14c94a75a702
KoryTECH <korylinemedia@gmail.com>
2026-01-22 10:49:12 +0100

    added a little documentation and the hourly display is succesful

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const OPENCAGE_KEY = `75caf9cfcc1c44f2914802b3d1cd08aa`;
 let lat;
 let lon;
 const encodedLocation = `lagos`
+// this function has the api that gets the location entered in the input section then gets the latitude and longitude, then update the longitude and latitude variable
 function getnewLonAndLat(){
   fetch(`https://api.opencagedata.com/geocode/v1/json?q=${encodedLocation}&key=${OPENCAGE_KEY}`)
   .then(response => response.json())
@@ -14,7 +15,7 @@ function getnewLonAndLat(){
 }
 
 // getnewLonAndLat();
-
+// function to fetch and display data from openweather api
 function fetchAndDisplay(){
   fetch(`${OPENWEATHER_URL}?lat=${lat}&lon=${lon}&appid=${OPENWEATHER_KEY}&units=metric`)
         .then(response => response.json())
@@ -25,30 +26,36 @@ function fetchAndDisplay(){
             const minRange = data.list[0].main.temp_min;
             const maxRange = data.list[0].main.temp_max;
             const description = data.list[0].weather[0].description
-            
-            document.getElementById('main-temp').innerText  = data.list[0].main.temp;
+            const temIcon = data.list[0].weather[0].icon
+            // the display of the main information
+            document.getElementById('main-temp').innerText  = `${data.list[0].main.temp}℃`;
             document.getElementById('location').innerText = locationName;
             document.getElementById('date').innerText = date;
             document.getElementById('temp-description').innerText = description;
-            document.getElementById('temp-range').innerText = `H:${maxRange} L:${minRange}`;
-
+            document.getElementById('temp-icon').innerHTML = `<img src="https://openweathermap.org/img/wn/${temIcon}@2x.png" alt="${description}">`;            document.getElementById('temp-range').innerText = `H:${maxRange} L:${minRange}`;
+            // giving a variable to the data List
             let list = data.list;
             console.log(list)
+            // gets the first five object in the array and store them in an array called hours
             let hours = [];
             for (let i = 0; i < 5; i++) {
               hours.push(list[i]);
             }
+            // gets the five days out of the response from the api
             let days = [];
             for(let index = 0; index < 39; index+=5 ){
               days.push(list[index])
             }
-            let hourlyHtml;
+            // displaying the hourly data
+            let hourlyHtml='';
                 for(let x = 0; x<5; x++){
+                  const iconCode = hours[x].weather[0].icon;
+                  const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
                   hourlyHtml += `
                     <div class="hour">
                       <div class="current">${hours[x].dt_txt.split(' ')[1]}</div>
-                      <div class="current hourly weather-description">${hours[x].weather[0].description}</div>
-                      <div class="current">${hours[x].main.temp}</div>
+                      <div class="current hourly weather-icon"><img src="${iconUrl}" alt="${hours[x].weather[0].description}"></div>
+                      <div class="current">${hours[x].main.temp}℃</div>
                   </div>
               `;
               document.querySelector('.five-hours-forecast').innerHTML = hourlyHtml
@@ -67,7 +74,7 @@ function fetchAndDisplay(){
             
         })
 }
-
+// getting the user location(longitude and latitude) onload to fetch the weather info on load and updates the longitude and latitude variable
 window.onload = function () {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
